bf19ecbe350d3559411461117d0b7a48bb1e1906
BetoSandoval <alberto.sa1391@gmail.com>
2021-10-27 17:45:58 -0500

    procesando datos y mostrandolos en consola

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -2,16 +2,18 @@ import React, { useEffect, useState } from "react";
 import { ItemCount } from "./ItemCount";
 import styled from "styled-components";
 import ItemList from "./ItemList";
+import itemData from '../assets/Data/items';
 
 // itemListContainer es un Componente destinado a conseguir la logica del catalogo (una array de productos actualiza el estado y pasa los datos a itemList)
 const ItemListContainer = (props) => {
+  const fakeApi = itemData;
   const [items, setItems] = useState([]);
 
-  const peticion = () => {
+  const peticion = ( fakeApi ) => {
     return new Promise((resolve, reject) => {
-      if (items.length !== 0) {
+      if (fakeApi.length !== 0) {
         setTimeout(() => {
-          resolve(true);
+          resolve(fakeApi);
         }, 2000);
       } else {
         const error = new Error("No se pudo conectar con los datos");
@@ -22,11 +24,12 @@ const ItemListContainer = (props) => {
 
 
   useEffect(() => {
-    peticion(  )
-    .then((response) => console.log(response))
+    peticion( fakeApi )
+    .then(data => setItems(data))
     .catch( err => console.error(err));
-  }, [])
+  }, [fakeApi])
 
+  console.log(items);
 
   return (
     <Main>
