9355fe54ed2a82df92f395b0423b8290a0708f96
BetoSandoval <alberto.sa1391@gmail.com>
2021-10-27 16:17:24 -0500

    Archivo con objeto JSON

---
+++ src/assets/Data/items.js
@@ -0,0 +1,20 @@
+const projects = [
+    {
+        id: 1,
+        name: 'planta suave',
+        desc: 'lorem ipsun dolor',
+        stock: 25
+    },
+    {
+        id: 2,
+        name: 'planta dura',
+        desc: 'lorem ipsun dolor',
+        stock: 10
+    },
+    {
+        id: 3,
+        name: 'planta verde',
+        desc: 'lorem ipsun dolor',
+        stock: 2
+    }
+];
