f4adae16819ad07b4b1a133f342e9f6a88cd6e45
BetoSandoval <alberto.sa1391@gmail.com>
2021-10-27 16:17:05 -0500

    Cree los componentes ItemList e Item

---
+++ src/components/ItemList.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Item from './Item'
+
+// Agrupa y mapea items
+const ItemList = () => {
+    return (
+        <div>
+            <Item/>
+        </div>
+    )
+}
+
+export default ItemList
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { ItemCount } from "./ItemCount";
 import styled from "styled-components";
+import ItemList from "./ItemList";
 
-
+// itemListContainer es un Componente destinado a conseguir la logica del catalogo (una array de productos actualiza el estado y pasa los datos a itemList)
 const ItemListContainer = (props) => {
-
+  
   return (
     <Main>
       <div className="main-container">
         <h1 className="main-title">{props.greeting}</h1>
+        {/* Crear dos componentes más itemList e item, Item list itera a item el numero de veces */}
+        <ItemList/>
+
 
         <ItemCount stock={5} initial={0}/>
       </div>
