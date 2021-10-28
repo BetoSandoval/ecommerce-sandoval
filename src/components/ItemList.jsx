0b01650ec19874226947379b5397165f5623095d
BetoSandoval <alberto.sa1391@gmail.com>
2021-10-27 19:10:53 -0500

    Aplique el metodo map para renderizar los items

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,14 +1,27 @@
-import React from 'react'
-import Item from './Item'
+import React from 'react';
+import Item from './Item';
+import styled from "styled-components";
 
 // Agrupa y mapea items
 const ItemList = (props) => {
     return (
-        <div>
-            <h1>{JSON.stringify(props.dataItems)}</h1>
-            <Item/>
-        </div>
+        <ItemCard>
+            {props.dataItems.map( (item) => (
+                <Item
+                key={item.id}
+                name={item.name}
+                description={item.desc}
+                stock={item.stock}
+            />
+            ))}
+        </ItemCard>
     )
 }
+
+const ItemCard = styled.div`
+    display: flex;
+    justify-content: space-between;
+    margin-top: 2rem;
+`;
 
 export default ItemList;
