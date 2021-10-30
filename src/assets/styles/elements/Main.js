a8feccb4fb13230c4527e51baee46066dd6292ea
BetoSandoval <alberto.sa1391@gmail.com>
2021-10-29 19:09:47 -0500

    Cree el elemento Main.js y agregue los estilos ahi

---
+++ src/assets/styles/elements/Main.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const Main = styled.main`
+  .main-container {
+    margin-right: auto;
+    margin-left: auto;
+    width: 117rem;
+    padding: 0 1.5rem;
+    .main-title {
+      text-align: center;
+    }
+  }
+`;
+
+export default Main;
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from "react";
-import styled from "styled-components";
 import ItemList from "./ItemList";
 import itemData from '../assets/Data/items';
-
+import Main from '../assets/styles/elements/Main';
 // itemListContainer es un Componente destinado a conseguir la logica del catalogo (una array de productos actualiza el estado y pasa los datos a itemList)
 const ItemListContainer = (props) => {
   const fakeApi = itemData;
@@ -28,27 +27,27 @@ const ItemListContainer = (props) => {
     .catch( err => console.error(err));
   }, [fakeApi])
 
-  return (
-    <Main>
-      <div className="main-container">
-        <h1 className="main-title">{props.greeting}</h1>
-        {/* Crear dos componentes más itemList e item, Item list itera a item el numero de veces */}
-        <ItemList dataItems={items}/>
-      </div>
-    </Main>
-  );
+  if( items.length === 0 ){
+    return(
+      <Main>
+        <div className="main-container">
+          <h1 className="main-title">Cargando productos...</h1>
+          </div>
+      </Main>
+    )
+  } else{
+    return (
+      <Main>
+        <div className="main-container">
+          <h1 className="main-title">{props.greeting}</h1>
+          {/* Crear dos componentes más itemList e item, Item list itera a item el numero de veces */}
+          <ItemList dataItems={items}/>
+        </div>
+      </Main>
+    ); 
+  }
 };
 
-const Main = styled.main`
-  .main-container {
-    margin-right: auto;
-    margin-left: auto;
-    width: 117rem;
-    padding: 0 1.5rem;
-    .main-title {
-      text-align: center;
-    }
-  }
-`;
+
 
 export default ItemListContainer;
