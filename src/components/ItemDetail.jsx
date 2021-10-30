36607790740fe903759e60d0e700c8a54351b380
BetoSandoval <alberto.sa1391@gmail.com>
2021-10-29 19:13:43 -0500

    Cree itemDetail

---
+++ src/components/ItemDetail.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import styled from "styled-components";
+import { ItemCount } from "./ItemCount";
+
+const ItemDetail = (props) => {
+  return (
+    <ItemDetailContainer>
+      <figure>
+        <img src={props.item.image} alt="Imagen del producto" />
+      </figure>
+
+      <div className="item-info">
+        <ItemCount 
+          name={props.item.title} 
+          stock={props.item.rating.count} 
+          initial={0}
+          description = {props.item.description}
+        />
+        <p>
+          <span>Price:</span> {props.item.price}
+        </p>
+      </div>
+    </ItemDetailContainer>
+  );
+};
+
+const ItemDetailContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+  figure {
+    width: 50%;
+    img {
+      width: 40rem;
+    }
+  }
+
+  .item-info {
+    width: 50%;
+    text-align: center;
+    margin: 24rem 2rem;
+  }
+`;
+
+export default ItemDetail;
