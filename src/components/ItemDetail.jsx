3961f39f04c7e18dbc435ec370176f432e6b879b
BetoSandoval <alberto.sa1391@gmail.com>
2021-11-06 14:32:38 -0600

    Agregue una funcion que guarda datos del child

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -1,33 +1,42 @@
-import React from "react";
+import React,{useState} from "react";
 import styled from "styled-components";
 import { ItemCount } from "./ItemCount";
-
+import { Link } from "react-router-dom";
 
 const ItemDetail = (props) => {
-  const stateUpliftingfronCounter = ( datachild ) => {
-    console.log('Soy un metodo invocado en el hijo pero me ejecuto en el padre')
-    console.log(datachild)
-  }
+  const [count, setCount] = useState(0)
+
+  const stateUpliftingfronCounter = (datachild) => {
+    setCount(datachild)
+  };
+
+  console.log(`Se agregaron ${count} producots al carrito`);
 
   return (
-      <ItemDetailContainer>
-        <figure>
-          <img src={props.item.image} alt="Imagen del producto" />
-        </figure>
+    <ItemDetailContainer>
+      <figure>
+        <img src={props.item.image} alt="Imagen del producto" />
+      </figure>
 
-        <div className="item-info">
-          <h3 className="title-item">{props.item.title}</h3>
-          <ItemCount
+      <div className="item-info">
+        <h3 className="title-item">{props.item.title}</h3>
+        <p className="price">Price: $ {props.item.price}</p>
+        <p className="desc">{props.item.description}</p>
+
+        {
+          count === 0 
+          ? <ItemCount
             stock={props.item.rating.count}
             initial={0}
             onClick={stateUpliftingfronCounter}
           />
+          : <Link to={"/cart"}>
+          <button>go to cart</button>
+        </Link> 
+        }
 
-          <p className="desc">{props.item.description}</p>
-
-          <p className="price">Price: $ {props.item.price}</p>
-        </div>
-      </ItemDetailContainer>
+      </div>
+    </ItemDetailContainer>
   );
 };
 
@@ -36,8 +45,13 @@ const ItemDetailContainer = styled.div`
   justify-content: space-around;
   figure {
     width: 50%;
+    position: relative;
     img {
       width: 40rem;
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
     }
   }
 
@@ -45,6 +59,12 @@ const ItemDetailContainer = styled.div`
     width: 50%;
     text-align: center;
     margin: 24rem 2rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    .title-item {
+      margin-bottom: 0.5rem;
+    }
     .desc {
       padding: 2rem;
       font-size: 2rem;
