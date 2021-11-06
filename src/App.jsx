4c96bde43d05579a7edb7cbbdde4b566f9b0f6a0
BetoSandoval <alberto.sa1391@gmail.com>
2021-11-06 14:34:18 -0600

    Rutas al elemento cart

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import ItemDetailContainer from "./components/ItemDetailContainer";
 import ItemListContainer from "./components/ItemListContainer";
+import Cart  from "./components/Cart";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
         <Route exact path="/" element={<ItemListContainer greeting='Our products'/>} />
         <Route exact path="/category/:category" element={<ItemListContainer/>} />
         <Route exact path="/item/:id" element={<ItemDetailContainer/>} />
+        <Route exact path="/cart" element={<Cart/>} />
       </Routes>
     </>
   );
