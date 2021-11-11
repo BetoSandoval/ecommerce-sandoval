968f76830760aab1ce4926f4399eff9debf61634
BetoSandoval <alberto.sa1391@gmail.com>
2021-11-10 22:35:45 -0600

    Declarando useContext para verificar valor sea arr

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
 import Headder from "../assets/styles/elements/Headder";
 import CarWidget from "./CarWidget";
 import { NavBar } from "./NavBar";
+import { CartContext } from "../context/CartContext";
 
 const Header = () => {
+  const result = useContext(CartContext);
+  console.log(result);
+
   return (
     <Headder id="main-header" className="header-v5 hidden-sm hidden-xs">
       <div className="container">
