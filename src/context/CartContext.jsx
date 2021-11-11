8ede545dcde8a1fe136e20c2160e4e12c6413e13
BetoSandoval <alberto.sa1391@gmail.com>
2021-11-10 23:00:21 -0600

    Dentro del archivo CartContext cree cart y addToCart forman un objeto que es el valor predeterminado del Provider

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -5,10 +5,19 @@ export const CartContext = createContext([]);
 const { Provider } = CartContext;
 
 const CustomComponent = ( {children} ) => {
-  const [cart, setCart] = useState([])
+  const [cart, setCart] = useState([]);
+
+  const addToCart = ( product, amount ) => {
+
+  }
+
+  const contextValue = {
+    cart: cart,
+    addToCart: addToCart
+  }
 
   return(
-    <Provider value={context_value}>
+    <Provider value={contextValue}>
         {children}
     </Provider>
   );
