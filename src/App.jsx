81923c19d75b8783be4a6cd172131fce9c076cc7
BetoSandoval <alberto.sa1391@gmail.com>
2021-11-10 22:14:14 -0600

    Creando customp componet y su provider

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,21 @@ import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import ItemDetailContainer from "./components/ItemDetailContainer";
 import ItemListContainer from "./components/ItemListContainer";
-import Cart  from "./components/Cart";
+import Cart from "./components/Cart";
+import CustomComponent from "./context/CartContext";
 
 const App = () => {
   return (
     <>
-      <Header />
-      <Routes>
-        <Route exact path="/" element={<ItemListContainer greeting='Our products'/>} />
-        <Route exact path="/category/:category" element={<ItemListContainer/>} />
-        <Route exact path="/item/:id" element={<ItemDetailContainer/>} />
-        <Route exact path="/cart" element={<Cart/>} />
-      </Routes>
+      <CustomComponent>
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<ItemListContainer greeting="Our products" />}/>
+          <Route exact path="/category/:category" element={<ItemListContainer />}/>
+          <Route exact path="/item/:id" element={<ItemDetailContainer />} />
+          <Route exact path="/cart" element={<Cart />} />
+        </Routes>
+      </CustomComponent>
     </>
   );
 };
--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,3 +1,16 @@
-import { createContext } from 'react';
+import { createContext } from "react";
 
 export const CartContext = createContext([]);
+
+const { Provider } = CartContext;
+
+const CustomComponent = ( {children} ) => {
+    const context_value = [];
+  return(
+    <Provider value={context_value}>
+        {children}
+    </Provider>
+  );
+};
+
+export default CustomComponent;
