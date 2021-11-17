fc73d8f378f66f70dc914ec65a6db9aca9a3ce8b
BetoSandoval <alberto.sa1391@gmail.com>
2021-11-16 21:14:21 -0600

    contexto, renderizado condicional y delete btn

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { CartContext } from "../context/CartContext";
+import { Link } from "react-router-dom";
 
 const Cart = () => {
-    return (
-        <div>
-            <h1>Here is going to be the cart section!</h1>
-        </div>
-    )
-}
+  const { cart, deleteToCartContext } = useContext(CartContext);
+  console.log(cart.length);
+  const deleteItem = (e) => {
+      e.targe
 
-export default Cart
+  }
+
+  return (
+    <div>
+      {cart.length ? (
+        cart.map((index) => (
+          <article key={index.product.item.id}>
+            <h3>{index.product.item.title}</h3>
+            <p>
+              Total : $ {index.product.item.price * index.amount} (Cant :{" "}
+              {index.amount})
+            </p>
+            <button onClick={() => deleteToCartContext(index.product.item.id)}>Delete</button>
+          </article>
+        ))
+      ) : (
+        <>
+          <h3> The cart is empty </h3>
+          <Link to="/">
+            <button>Take a look to our products</button>
+          </Link>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Cart;
