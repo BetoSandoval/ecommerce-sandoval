86c7ba572afe7b8b15f3ffd46d7467a283ed0490
BetoSandoval <alberto.sa1391@gmail.com>
2021-11-16 21:12:01 -0600

    Elimine context de header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,10 @@
-import React, { useContext } from "react";
+import React from "react";
 import Headder from "../assets/styles/elements/Headder";
 import CarWidget from "./CarWidget";
 import { NavBar } from "./NavBar";
-import { CartContext } from "../context/CartContext";
+
 
 const Header = () => {
-  const { cart } = useContext(CartContext);
-  console.log(cart);
 
   return (
     <Headder id="main-header" className="header-v5 hidden-sm hidden-xs">
