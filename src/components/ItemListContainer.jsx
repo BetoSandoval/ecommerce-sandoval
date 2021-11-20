1ec0f2a7b34b7ea2dc8609776f554daa60677ad4
BetoSandoval <alberto.sa1391@gmail.com>
2021-11-19 20:59:01 -0600

    Llamada a la bd, coleccion, consulta, seteo state

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -7,23 +7,27 @@ import { firestore } from "../firebase/firebase";
 const ItemListContainer = (props) => {
   const { category } = useParams();
 
-  const [items, setItems] = useState([]);
-
+  const [products, setProducts] = useState([]);
+  //1) Llamar a la db
+  //2) Coleccion
+  //3) Hacer consulta
+  //4) Obetener resultados y verificar que todo este en orden
+  //5)Seteo el esatdo
   const db = firestore;
   const collection = db.collection("productos");
   const promesa = collection.get();
 
-
   promesa
-        .then((documento) => {
-          const data = documento.data();
-          console.log(data);
-        })
-        .catch(() => {
-          console.log("Hubo un error")
-        })
+    .then( (querySnapshot) => {
+      setProducts(querySnapshot.docs.map( doc => ({...doc.data(),id:doc.id}) ));
+      console.log(products);
+      }
+    )
+    .catch(() => {
+      console.log("Hubo un error");
+    });
 
-/*   useEffect(() => {
+  /*   useEffect(() => {
     if (category) {
       fetch(`https://fakestoreapi.com/products/category/${category}`)
         .then((res) => res.json())
@@ -35,7 +39,7 @@ const ItemListContainer = (props) => {
     }
   }, [category]); */
 
-  if (items.length === 0) {
+  if (products.length === 0) {
     return (
       <Main>
         <div className="main-container">
@@ -49,7 +53,7 @@ const ItemListContainer = (props) => {
         <div className="main-container">
           <h1 className="main-title">{props.greeting}</h1>
           {/* Crear dos componentes más itemList e item, Item list itera a item el numero de veces */}
-          <ItemList dataItems={items} />
+          <ItemList dataItems={products} />
         </div>
       </Main>
     );
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -7,7 +7,7 @@ const firebaseConfig = {
   projectId: "ecomerce-coderhouse",
   storageBucket: "ecomerce-coderhouse.appspot.com",
   messagingSenderId: "766357322002",
-  appId: "1:766357322002:web:afbb3471d0df9910259bcb",
+  appId: "1:766357322002:web:afbb3471d0df9910259bcb"
 };
 
 const app = firebase.initializeApp( firebaseConfig );
