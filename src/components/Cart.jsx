036081d75c85fbf2ababbc955b0d2594d98329f4
BetoSandoval <alberto.sa1391@gmail.com>
2021-11-21 17:58:43 -0600

    Estilos de cart.jsx

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,38 +1,71 @@
 import React, { useContext } from "react";
 import { CartContext } from "../context/CartContext";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 const Cart = () => {
   const { cart, deleteToCartContext } = useContext(CartContext);
 
-  const handleDelete = (id) =>{
+  const handleDelete = (id) => {
     deleteToCartContext(id);
-  }
- 
+  };
+  console.log(cart);
 
   return (
     <div>
       {cart.length ? (
         cart.map((index) => (
-          <article key={index.product.item.id}>
-            <h3>{index.product.item.title}</h3>
-            <p>
-              Total : $ {index.product.item.price * index.amount} (Cant :{" "}
-              {index.amount})
-            </p>
-            <button onClick={() => handleDelete( index.product.item.id)}>Delete</button>
-          </article>
+          <Product key={index.product.item.id}>
+            <img src={index.product.item.image} alt="imagen del producto" />
+            <div className="info-container">
+              <h3>{index.product.item.title}</h3>
+              <p>
+                Total : $ {index.product.item.price * index.amount} (Cant :{" "}
+                {index.amount})
+              </p>
+              <button onClick={() => handleDelete(index.product.item.id)}>
+                Delete
+              </button>
+            </div>
+          </Product>
         ))
       ) : (
-        <>
-          <h3> The cart is empty </h3>
+        <Main>
+          <h3 className="title"> The cart is empty </h3>
           <Link to="/">
             <button>Take a look to our products</button>
           </Link>
-        </>
+        </Main>
       )}
     </div>
   );
 };
+
+const Product = styled.article`
+  width: 69.5rem;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  border: 2px solid #2b2b2b;
+  border-radius: 3px;
+  margin-left: 4rem;
+  padding: 2rem;
+  margin-bottom: 1rem;
+  img {
+    max-width: 13rem;
+  }
+  .info-container{
+    display: flex;
+    flex-direction:column;
+    align-items: center;
+  }
+`;
+
+const Main = styled.article`
+  text-align:center;
+  .title{
+    font-size: 4rem;
+  }
+`;
 
 export default Cart;
