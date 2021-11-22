e0a4a8bf54ced36e4d525f1a917eb8ed0446bb45
BetoSandoval <alberto.sa1391@gmail.com>
2021-11-21 20:44:42 -0600

    Estilos del numero del carrito

--- src/components/CarWidget.jsx
+++ src/components/CarWidget.jsx
@@ -9,8 +9,8 @@ const CarWidget = () => {
   return (
     <Link to="/cart">
       <ShoppingCar>
+        {cart.length ? <Number>{cart.length}</Number> : null}
         <span className="material-icons">shopping_cart</span>
-        {cart.length ? cart.length : null}
       </ShoppingCar>
     </Link>
   );
@@ -19,11 +19,20 @@ const CarWidget = () => {
 const ShoppingCar = styled.div`
   border: 1px solid #e5e5e5;
   width: 10rem;
-  margin-top: 2rem;
+  margin-top: 1rem;
   .material-icons {
     color: #80b435;
     padding: 0.5rem 3.8rem;
   }
+`;
+
+const Number = styled.p`
+  width: 10rem;
+  margin-top: 2rem;
+  text-align: center;
+  background-color: red;
+  color: white;
+  border-radius: 5rem;
 `;
 
 export default CarWidget;
